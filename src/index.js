18ab7f3e716bfec1284967b5c1c3aac40870f605
Anton Kolomiiets <resdenia@gmail.com>
2021-03-20 13:11:14 +0200

    added field and simple validation

--- src/index.js
+++ src/index.js
@@ -14,16 +14,26 @@ class ViewCardForm {
   createField(id, name, labelText, type) {
     const fieldWrapper = document.createElement('div');
     const fieldLabel = document.createElement('label');
-    const fieldElement = document.createElement('input');
 
     fieldWrapper.classList.add('form-control');
     fieldLabel.textContent = labelText;
-    fieldElement.setAttribute('id', id);
-    fieldElement.setAttribute('name', name);
-    fieldElement.setAttribute('type', type);
-
     fieldWrapper.appendChild(fieldLabel);
-    fieldWrapper.appendChild(fieldElement);
+
+    if (id === 'experationDate') {
+      const fieldElementMonth = document.createElement('select');
+      const fieldElementYear = document.createElement('select');
+      const expiretionContainer = document.createElement('div');
+      expiretionContainer.appendChild(fieldElementMonth);
+      expiretionContainer.appendChild(fieldElementYear);
+      expiretionContainer.classList.add('exp-date');
+      fieldWrapper.appendChild(expiretionContainer);
+    } else {
+      const fieldElement = document.createElement('input');
+      fieldElement.setAttribute('id', id);
+      fieldElement.setAttribute('name', name);
+      fieldElement.setAttribute('type', type);
+      fieldWrapper.appendChild(fieldElement);
+    }
     return fieldWrapper;
   }
 
@@ -68,12 +78,44 @@ class ViewCardForm {
   }
 
   onValidateName() {}
-  onValidateCVV() {}
+  onValidateCVV() {
+    const cvvField = document.querySelector(`#${this.cardInfo[3].id}`);
+    cvvField.setAttribute('maxlength', '4');
+
+    cvvField.addEventListener('keyup', (e) => {
+      if (!e.target.value.match(/^\d{3,4}$/)) {
+        this.errorMessage('CVV not valid', e.target);
+      } else {
+        const formWrapper = e.target.closest('.form-control');
+        if (formWrapper.querySelector('.error-container')) {
+          formWrapper.querySelector('.error-container').remove();
+        }
+      }
+    });
+  }
   onValidateExpDate() {}
-  onValidateNumberCard() {}
+  onValidateNumberCard() {
+    //regex ^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$
+  }
+
+  errorMessage(message, el) {
+    const formWrapper = el.closest('.form-control');
+    if (!formWrapper.querySelector('.error-container')) {
+      const errorWrapper = document.createElement('div');
+
+      errorWrapper.classList.add('error-container');
+      errorWrapper.textContent = message;
+
+      formWrapper.appendChild(errorWrapper);
+    }
+  }
+  init() {
+    this.initStructure();
+    this.initWrapperRepresentor();
+    this.onValidateCVV();
+  }
 }
 
 const viewCardInit = new ViewCardForm('#root');
 
-viewCardInit.initStructure();
-viewCardInit.initWrapperRepresentor();
+viewCardInit.init();
